3bea7aa2845674d5b38e2062ca235124d90e8595
Berkeley Martinez <Berkeley@robotie.com>
2015-04-16 21:02:51 -0700

    Begin context wrapper testing.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var Actions = require('./lib/Actions'),
     Cat = require('./lib/Cat'),
     Store = require('./lib/Store'),
+    Container = require('./lib/Container'),
     waitFor = require('./lib/waitFor');
 
 module.exports = {
   Actions: Actions,
   Cat: Cat,
+  Container: Container,
   Store: Store,
   waitFor: waitFor
 };
---
+++ test/contextWrapper.js
@@ -0,0 +1,97 @@
+process.env.NODE_ENV = 'development';
+/* eslint-disable no-unused-expressions */
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import React from 'react/addons';
+import ContextWrapper from '../lib/ContextWrapper';
+import { Cat } from '../';
+
+const ReactTestUtils = React.addons.TestUtils;
+
+chai.should();
+chai.use(sinonChai);
+
+describe('ContextWrapper', function() {
+  describe('wrap', function() {
+    it('should throw if given a non react component', function() {
+      expect(() => {
+        ContextWrapper.wrap('not the momma');
+      }).to.throw(/expects a valid React element/);
+    });
+
+    it('should throw if not given a cat instance', function() {
+      expect(() => {
+        ContextWrapper.wrap(React.createElement('div'), 'not the momma');
+      }).to.throw(/expects an instance of Cat/);
+    });
+
+    it('should return a valid react element', function() {
+      let catApp = new Cat();
+      let Burrito = ContextWrapper.wrap(React.createElement('div'), catApp);
+      React.isValidElement(Burrito).should.be.true;
+    });
+
+  });
+
+  describe('component', function() {
+    let catApp, Burrito;
+    beforeEach(function() {
+      catApp = new Cat();
+    });
+
+    it('should throw if given more than one child', function() {
+      expect(() => {
+        ReactTestUtils.renderIntoDocument(React.createElement(
+          ContextWrapper,
+          { cat: catApp },
+          React.createElement('div'),
+          React.createElement('div')
+        ));
+      }).to.throw();
+    });
+
+    it('should warn if cat is not an object', function() {
+      // This test is brittle
+      // latest version of react use console.error this test may need to change
+      // with next release
+      let spy = sinon.spy(console, 'warn');
+      ReactTestUtils.renderIntoDocument(React.createElement(
+        ContextWrapper,
+        { cat: 'notTheMomma' },
+        React.createElement('div')
+      ));
+      spy.restore();
+      spy.should.have.been.called;
+    });
+
+    it('should add context object to child component', function() {
+      const spy = sinon.spy();
+      class TestComp extends React.Component {
+        constructor(props) {
+          super(props);
+        }
+        componentWillMount() {
+          console.log(this.context);
+          spy(this.context.cat);
+        }
+        render() {
+          return null;
+        }
+      }
+
+      TestComp.contextTypes = {
+        cat: React.PropTypes.object.isRequired
+      };
+
+      Burrito = ContextWrapper.wrap(React.createElement(TestComp), catApp);
+      ReactTestUtils.renderIntoDocument(Burrito);
+      spy.should.have.been.calledOnce;
+      spy.should.have.been.calledWith(catApp);
+    });
+
+    it('should render its children', function() {
+    });
+  });
+});
